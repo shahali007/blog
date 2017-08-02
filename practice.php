64f19ccad7d4f327fd85e8f5c7feb6e87a971564
182111 <shahali@jagonews24.com>
2017-08-02 11:04:33 +0600

    updated

--- practice.php
+++ practice.php
@@ -8,10 +8,20 @@
     <title>Test file</title>
 </head>
 <body>
-
+    <button type="submit" id="info" onclick="getterText()">
+        Lorem ipsum dolor sit amet.
+    </button>
 </body>
 </html>
 <script>
+
+    // get inner text of a div
+    function getterText() {
+        //var inner = document.getElementById(info).innerHTML;
+        alert("hskd");
+    }
+
+    // for sum of array value
     var numbers = [10,20,25,30,55,42];
     function sum(n) {
         var sumation = 0;
