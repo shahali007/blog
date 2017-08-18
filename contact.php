da273885bed67db0f7fa40d4332e6853d83e0848
182111 <shahali@jagonews24.com>
2017-08-18 22:02:24 +0600

    updated files

--- contact.php
+++ contact.php
@@ -2,8 +2,20 @@
     include_once "header.php";
     if ($_SERVER['REQUEST_METHOD'] && isset($_POST['msg_submit'])){
        $msgSend = $blogInfo->msgSent($_POST);
+
+        foreach($msgSend as $key => $error){
+            $sss = $error;
+        }
+        if($key == 'err_first_name'){echo $sss;}
+
+        //echo $sss[0];
+        //print_r($sss);
+        die();
     }
     $Send = Session::get("MsgSubmit");
+
+
+
 ?>
     <div class="main">
         <div class="row">
@@ -12,8 +24,21 @@
                     <div class="about">
                         <h3 class="heading">Stay connect with us</h3>
                         <?php if($Send != NULL){
-                            echo '<script>alert($Send)</script>';
+                            echo $Send;
                         } Session::set("MsgSubmit", NULL) ?>
+                        <hr>
+                        <?php if(isset($msgSend)){?>
+                            <div class="text-center">
+                                <?php
+                                foreach($msgSend as $error){?>
+                                    <div class="alert alert-danger alert-dismissable" style="background: red;color: #fff;border: 1px solid red;padding: 5px 30px 5px 5px ;margin-bottom: 5px;">
+                                        <button type="button" class="close" style="color:#000;" data-dismiss="alert" aria-hidden="true">×</button>
+                                        <?php echo $error."<br/>";?>
+                                    </div>
+                                <?php } ?>
+                            </div>
+                        <?php } ?>
+                        <hr>
                         <form action="" method="POST" class="form-horizontal" role="form">
                             <div class="form-group">
                                 <label class="col-sm-3" for="first_name">Your First Name</label>
